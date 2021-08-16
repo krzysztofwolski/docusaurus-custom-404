51f00e4c36849b39f86f77834d71e274f1d2920e
krzysztofwolski <krzysztof.k.wolski@gmail.com>
2021-08-16 23:20:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-yw96jb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-yw96jb)
---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+const React = require('react');
+
+const Index = () => {
+  console.log('xx');
+  return <p>This is custom 404</p>;
+};
+
+export default Index;
